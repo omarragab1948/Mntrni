66f434fcfea403bf587a80494d0b594a0d6de841
Omar Mohamed <70483549+omarragab1948@users.noreply.github.com>
2023-11-24 16:16:59 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -93,14 +93,9 @@ const App = props => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        {/* <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title> */}
         <title>Mntrni</title>
 
-        <meta
-          name='description'
-          // content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-          content={`Mntrni is فhe best site for giving consultations`}
-        />
+        <meta name='description' content={`Mntrni is فhe best site for giving consultations`} />
         <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
